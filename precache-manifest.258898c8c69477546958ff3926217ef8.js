281b564b5de11ef7b942e765a0a9153c3432e51e
DConstantine9 <constantinedeniskkin@gmail.com>
2020-12-06 16:39:59 +0300

    Initial dist subtree commit

---
+++ precache-manifest.258898c8c69477546958ff3926217ef8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c9474cf150e106e8fb",
+    "url": "/beer-vue/css/app.468a521f.css"
+  },
+  {
+    "revision": "1d429086c7d850b233106e1aa2270e9f",
+    "url": "/beer-vue/index.html"
+  },
+  {
+    "revision": "25c9474cf150e106e8fb",
+    "url": "/beer-vue/js/app.4ecb1d93.js"
+  },
+  {
+    "revision": "55820d34aaee8e8fd5d0",
+    "url": "/beer-vue/js/chunk-vendors.ad246cbe.js"
+  },
+  {
+    "revision": "655a858d77984f207ae5989877f7bfe9",
+    "url": "/beer-vue/manifest.json"
+  }
+]);
